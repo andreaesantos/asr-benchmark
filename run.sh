a2505c759fa0e1af7ccd7cd27f9c9a1d83d7993c
andreaesantos <andrea98@me.com>
2026-06-23 11:59:20 +0200

    declare model envs run w/ conda; tbc - might be better to use uv

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
-# Instead of an associative array, use two parallel arrays
-MODELS=("whisperx") # "omni") #TODO: add vibevoice when dialogue formatting is implemented
-PATHS=(
-    "/Users/andreasantos/anaconda3/envs/whisperx/bin/python"
-    # "/Users/andreasantos/anaconda3/envs/vibevoice/bin/python"
-    "/Users/andreasantos/anaconda3/envs/omni_asr/bin/python"
+declare -A MODEL_ENVS=(
+  [whisperx]="whisperx"
+  [omni]="omni_asr"
+  [vibevoice]="vibevoice"   
 )
 
-for i in "${!MODELS[@]}"; do
-    echo "--- Running benchmark for: ${MODELS[$i]} ---"
-    "${PATHS[$i]}" run_benchmark.py --model_filter "${MODELS[$i]}"
-    echo "--- Finished ${MODELS[$i]} ---"
+for model in "${!MODEL_ENVS[@]}"; do
+    echo "--- Running benchmark for: ${model} ---"
+    if conda run -n "${MODEL_ENVS[$model]}" python run_benchmark.py --model_filter "$model"; then
+        echo "--- Finished ${model} ---"
+    else
+        echo "!!! ${model} failed (exit $?) !!!" >&2
+        FAILED+=("$model")
+    fi
 done
+
+if (( ${#FAILED[@]} )); then
+    echo "Failed models: ${FAILED[*]}" >&2
+    exit 1
+fi
